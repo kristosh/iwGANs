04b188da49d3bd642154effb4cd0acd71078e313
chrathan <c.athanasiadis@maastrichtuniversity.nl>
2020-02-12 11:24:44 +0100

    data_handle as a class

--- data_handling.py
+++ data_handling.py
@@ -7,16 +7,6 @@ from keras.optimizers import SGD, Adagrad
 import cv2
 import os
 
-src_mod = "face"
-trg_mod = "audio"
-
-dacss_train_path = "../../GANs_cnn/models/_full_spec_new_train_112_28.pkl"
-dacss_test_path = "../../GANs_cnn/models/_full_spec_new_test_112_28.pkl"
-db_3d = "../../GANs_cnn/models/complete_3d.pkl"
-
-_indx_ = 200000
-_indx2_ = 215000
-
 
 def load_obj(filename):
     with open(filename, "rb") as fp:  # Unpickling
@@ -32,300 +22,245 @@ def _norm_(image):
     return (image.astype(np.float32) - 127.5)/127.5
 
 
-def get_data(datadir):
+class data_handle():
 
-    _dct_trn_ = load_obj(dacss_train_path)
+    def __init__(self):
+        self.src_mod = "face"
+        self.trg_mod = "audio"
 
-    _src_trn_ = np.transpose(_dct_trn_[src_mod+"_data"] , (0, 3, 1, 2))
+        self.dacss_train_path = "../../GANs_cnn/models/_full_spec_new_train_112_28.pkl"
+        self.dacss_test_path = "../../GANs_cnn/models/_full_spec_new_test_112_28.pkl"
+        self.db_3d = "../../GANs_cnn/models/complete.pkl"
+        self.wgans_path = "../models/complete.pkl"
 
-    _trg_trn_ = np.transpose(_dct_trn_[trg_mod+"_data"], (0, 3, 1, 2))
-    _lbls_trn_ = _dct_trn_[src_mod+"_lbls"]
+        self._indx_ = 200000
+        self._indx2_= 215000
 
-    _dct_tst_ = load_obj(dacss_test_path)
-    _src_tst_ = np.transpose(_dct_tst_[src_mod+"_data"], (0, 3, 1, 2))
-    _trg_tst_ = np.transpose(_dct_tst_[trg_mod+"_data"], (0, 3, 1, 2))
-    _lbls_tst_ = _dct_tst_[src_mod+"_lbls"]
+        self._lmt_1 = 350000
+        self._lmt_2 = 390000
+        self._lmt_3 = 450000
+        self._lmt_4 = 500000
 
-    _lbls_tst_ = np.reshape(_lbls_tst_, [_lbls_tst_.shape[0], 1])
-    _lbls_trn_ = np.reshape(_lbls_trn_, [_lbls_trn_.shape[0], 1])
-
-    _src_trn_ = _norm_(_src_trn_)    
-    _trg_trn_ = _norm_(_trg_trn_)
-
-    _src_tst_ = _norm_(_src_tst_)
-    _trg_tst_ = _norm_(_trg_tst_)
-
-    _src_vld_ = _src_trn_[_indx2_:]
-    _trg_vld_ = _trg_trn_[_indx2_:]
-    _lbls_vld_ = _lbls_trn_[_indx2_:]
-
-    _src_trn_ = _src_trn_[:_indx_]
-    _trg_trn_ = _trg_trn_[:_indx_]
-    _lbls_trn_ = _lbls_trn_[:_indx_]
-
-    _rndz_ = np.arange(len(_src_trn_))
-    np.random.shuffle(_rndz_)
     
-    _src_trn_ = _src_trn_[_rndz_]
-    _trg_trn_ = _trg_trn_[_rndz_]
-    _lbls_trn_ = _lbls_trn_[_rndz_]
+    def _sft_crisp_lbl_(self, _my_arr_):
 
-    _rndz_ = np.arange(len(_src_vld_))
-    np.random.shuffle(_rndz_)
+        _my_arr_[_my_arr_ == 0] = 0.01
+        _my_arr_[_my_arr_ == 1] = 0.99
 
-    _src_vld_ = _src_vld_[_rndz_]
-    _trg_vld_ = _trg_vld_[_rndz_]
-    _lbls_vld_ = _lbls_vld_[_rndz_]
-
-    _lbls_trn_ = to_categorical(_lbls_trn_)
-    _lbls_trn_ = _lbls_trn_[:, 1:]
-    
-    _lbls_trn_[_lbls_trn_ == 0] = 0.01
-    _lbls_trn_[_lbls_trn_ == 1] = 0.99
-
-    _lbls_vld_ = to_categorical(_lbls_vld_)
-    _lbls_vld_ = _lbls_vld_[:, 1:]
-    
-    _lbls_vld_[_lbls_vld_ == 0] = 0.01
-    _lbls_vld_[_lbls_vld_ == 1] = 0.99
-
-    _lbls_tst_ = to_categorical(_lbls_tst_)
-    _lbls_tst_ = _lbls_tst_[:, 1:]
-
-    _lbls_tst_[_lbls_tst_ == 0] = 0.01
-    _lbls_tst_[_lbls_tst_ == 1] = 0.99
-
-    del _dct_trn_, _dct_tst_
-
-    _dct_ = {"_src_trn_": _src_trn_,
-        "_trg_trn_": _trg_trn_,
-        "_lbls_trn_": _lbls_trn_,
-        "_src_vld_": _src_vld_,
-        "_trg_vld_": _trg_vld_,
-        "_lbls_vld_": _lbls_vld_,
-        "_src_tst_": _src_tst_,
-        "_trg_tst_": _trg_tst_,
-        "_lbls_tst_": _lbls_tst_}
-
-    return _dct_
-
-def crossValidFiles(filename):
-    
-    #data = load_obj("models/complete_set2.pkl")
-    data = load_obj(filename) 
-    #{"face_train":pred_train,"train_lbls": Y_train, "specs_train": audio}
-    face= data["face_train"]
-    audio = data["specs_train"]
-    lbls = data["train_lbls"]
-
-    randomize = np.arange(len(face))
-    np.random.shuffle(randomize)
-    face = face[randomize]
-    audio = audio[randomize]
-    lbls = lbls[randomize]
-
-    return face, audio, lbls
+        return _my_arr_
 
 
-def temporal_feats(target, 
-        fold_indx, 
-        size_of_feats, 
-        feats_type, 
-        db_path):
+    def get_data(self, datadir):
 
-    _dt_ = load_obj(db_path+
-        feats_type+"_"+
-        str(size_of_feats)+"_"+
-        str(fold_indx)+
-        ".pkl")
+        _dct_trn_ = load_obj(self.dacss_train_path)
 
-    train_feats= _dt_["feats_train"]
-    train_target = _dt_["target_train"]
-    lbls_train = _dt_["lbls_train"]
+        _src_trn_ = np.transpose(_dct_trn_[self.src_mod+"_data"] , (0, 3, 1, 2))
 
+        _trg_trn_ = np.transpose(_dct_trn_[self.trg_mod+"_data"], (0, 3, 1, 2))
+        _lbls_trn_ = _dct_trn_[src_mod+"_lbls"]
 
-    _dt_ = load_obj(db_path+
-        feats_type+"_"+
-        str(size_of_feats)+"_"+
-        str(2)+
-        ".pkl")
+        _dct_tst_ = load_obj(dacss_test_path)
+        _src_tst_ = np.transpose(_dct_tst_[self.src_mod+"_data"], (0, 3, 1, 2))
+        _trg_tst_ = np.transpose(_dct_tst_[self.trg_mod+"_data"], (0, 3, 1, 2))
+        _lbls_tst_ = _dct_tst_[self.src_mod+"_lbls"]
 
-    test_feats= _dt_["feats_train"]
-    test_target = _dt_["target_train"]
-    lbls_test = _dt_["lbls_train"]
+        _lbls_tst_ = np.reshape(_lbls_tst_, [_lbls_tst_.shape[0], 1])
+        _lbls_trn_ = np.reshape(_lbls_trn_, [_lbls_trn_.shape[0], 1])
 
-    _dt_ = load_obj(db_path+
-        feats_type+"_"+
-        str(size_of_feats)+"_"+
-        str(2)+
-        ".pkl")
+        _src_trn_ = _norm_(_src_trn_)    
+        _trg_trn_ = _norm_(_trg_trn_)
 
-    valid_feats= _dt_["feats_train"]
-    valid_target = _dt_["target_train"]
-    lbls_valid = _dt_["lbls_train"]
+        _src_tst_ = _norm_(_src_tst_)
+        _trg_tst_ = _norm_(_trg_tst_)
 
-    _dct_ = {"train_feats": train_feats, 
-             "valid_feats": valid_feats, 
-             "test_feats": test_feats,
-             "train_target": train_target, 
-             "valid_target": valid_target, 
-             "test_target": test_target,
-             "lbls_train": lbls_train, 
-             "lbls_valid": lbls_valid, 
-             "lbls_test": lbls_test,
-             }
+        _src_vld_ = _src_trn_[self._indx2_:]
+        _trg_vld_ = _trg_trn_[self._indx2_:]
+        _lbls_vld_ = _lbls_trn_[self._indx2_:]
 
-    return _dct_
+        _src_trn_ = _src_trn_[:self._indx_]
+        _trg_trn_ = _trg_trn_[:self._indx_]
+        _lbls_trn_ = _lbls_trn_[:self._indx_]
 
-
-def load_3d_dataset(target):
-
-    if target == "audio":
-        data = load_obj(db_3d) 
-
-        trn_fts= data["face_train"][:350000]
-        trn_trg = data["specs_train"][:350000]
-        trn_lbls = data["train_lbls"][:350000]
-        trn_trg = _norm_(trn_trg)
-
-        vld_fts = data["face_train"][390000:450000]
-        vld_trg = data["specs_train"][390000:450000]
-        vld_lbls = data["train_lbls"][390000:450000]
+        _rndz_ = np.arange(len(_src_trn_))
+        np.random.shuffle(_rndz_)
         
-        vld_trg = _norm_(vld_trg)
+        _src_trn_ = _src_trn_[_rndz_]
+        _trg_trn_ = _trg_trn_[_rndz_]
+        _lbls_trn_ = _lbls_trn_[_rndz_]
 
-        tst_fts = data["face_train"][500000:]
-        tst_trg = data["specs_train"][500000:]
-        tst_lbls= data["train_lbls"][500000:]
-        tst_trg = _norm_(tst_trg)
+        _rndz_ = np.arange(len(_src_vld_))
+        np.random.shuffle(_rndz_)
+
+        _src_vld_ = _src_vld_[_rndz_]
+        _trg_vld_ = _trg_vld_[_rndz_]
+        _lbls_vld_ = _lbls_vld_[_rndz_]
+
+        _lbls_trn_ = to_categorical(_lbls_trn_)
+        _lbls_trn_ = _lbls_trn_[:, 1:]
         
-        randomize = np.arange(len(trn_fts))
-        np.random.shuffle(randomize)
-        trn_fts = trn_fts[randomize]
-        trn_trg = trn_trg[randomize]
-        trn_lbls = trn_lbls[randomize]
+        _lbls_trn_ = self._sft_crisp_lbl_(_lbls_trn_)
 
-        trn_lbls[trn_lbls == 0] = 0.01
-        trn_lbls[trn_lbls == 1] = 0.99
+        _lbls_vld_ = to_categorical(_lbls_vld_)
+        _lbls_vld_ = _lbls_vld_[:, 1:]
+        
+        _lbls_vld_ = self._sft_crisp_lbl_(_lbls_vld_)
 
-        tst_lbls[tst_lbls == 0] = 0.01
-        tst_lbls[tst_lbls == 1] = 0.99
+        _lbls_tst_ = to_categorical(_lbls_tst_)
+        _lbls_tst_ = _lbls_tst_[:, 1:]
 
-        vld_lbls[tst_lbls == 0] = 0.01
-        vld_lbls[tst_lbls == 1] = 0.99
-       
-        _dct_ = {"trn_fts": trn_fts, 
-             "trn_trg": trn_trg, 
-             "trn_lbls": trn_lbls,
-             "vld_fts": vld_fts, 
-             "vld_trg": vld_trg, 
-             "vld_lbls": vld_lbls,
-             "tst_fts": tst_fts, 
-             "tst_trg": tst_trg, 
-             "tst_lbls": tst_lbls,
-             }
+        _lbls_tst_ = self._sft_crisp_lbl_(_lbls_tst_)
+        
+        del _dct_trn_, _dct_tst_
+
+        _dct_ = {"_src_trn_": _src_trn_,
+            "_trg_trn_": _trg_trn_,
+            "_lbls_trn_": _lbls_trn_,
+            "_src_vld_": _src_vld_,
+            "_trg_vld_": _trg_vld_,
+            "_lbls_vld_": _lbls_vld_,
+            "_src_tst_": _src_tst_,
+            "_trg_tst_": _trg_tst_,
+            "_lbls_tst_": _lbls_tst_}
 
         return _dct_
 
-    
-    elif target == "face":
 
-        data = load_obj("models/extrAudioFeats64.pkl") 
 
-        trn_feats= data["trainFeats"]
-        trn_tgt = data["face_train"]
-        trn_lbls = data["train_lbls"]
-    
-        tst_fts= data["testFeats"]
-        tst_trg = data["face_test"]
-        
-        tst_lbls= data["test_lbls"]
-        
-        randomize = np.arange(len(trn_feats))
-        np.random.shuffle(randomize)
-        trn_feats = trn_feats[randomize]
-        trn_tgt = trn_tgt[randomize]
-        trn_lbls = trn_lbls[randomize]
+    def temporal_feats(
+            self,
+            target, 
+            fold_indx, 
+            size_of_feats, 
+            feats_type, 
+            db_path):
 
-        trn_lbls[trn_lbls == 0] = 0.01
-        trn_lbls[trn_lbls == 1] = 0.99
+        _dt_ = load_obj(db_path+
+            feats_type+"_"+
+            str(size_of_feats)+"_"+
+            str(fold_indx)+
+            ".pkl")
 
-        tst_lbls[tst_lbls == 0] = 0.01
-        tst_lbls[tst_lbls == 1] = 0.99
+        train_feats= _dt_["feats_train"]
+        train_target = _dt_["target_train"]
+        lbls_train = _dt_["lbls_train"]
 
-        trn_tgt = trn_tgt.transpose(0,2,3,1)
-        tst_trg = tst_trg.transpose(0,2,3,1)
 
-        _dct_ = {"trn_feats": trn_feats, 
-             "trn_tgt": trn_tgt, 
-             "trn_lbls": trn_lbls,
-             "tst_fts": tst_fts, 
-             "tst_trg": tst_trg, 
-             "tst_lbls": tst_lbls,
-             }
-        
+        _dt_ = load_obj(db_path+
+            feats_type+"_"+
+            str(size_of_feats)+"_"+
+            str(2)+
+            ".pkl")
+
+        test_feats= _dt_["feats_train"]
+        test_target = _dt_["target_train"]
+        lbls_test = _dt_["lbls_train"]
+
+        _dt_ = load_obj(db_path+
+            feats_type+"_"+
+            str(size_of_feats)+"_"+
+            str(2)+
+            ".pkl")
+
+        valid_feats= _dt_["feats_train"]
+        valid_target = _dt_["target_train"]
+        lbls_valid = _dt_["lbls_train"]
+
+        _dct_ = {"train_feats": train_feats, 
+                "valid_feats": valid_feats, 
+                "test_feats": test_feats,
+                "train_target": train_target, 
+                "valid_target": valid_target, 
+                "test_target": test_target,
+                "lbls_train": lbls_train, 
+                "lbls_valid": lbls_valid, 
+                "lbls_test": lbls_test,
+                }
+
         return _dct_
 
 
-def load_3d_iwGANs():
+    def load_3d_dataset(self, target):
 
-    data = load_obj("../models/complete.pkl") 
+        if target == "audio":
+            
+            _dt_ = load_obj(self.db_3d)
 
-    face_train= data["face_train"]
-    spec_train = data["specs_train"]
-    lbls_train = data["train_lbls"]
-    spec_train = (spec_train.astype(np.float32) - 127.5) / 127.5
-    
-    face_test= data["face_test"]
-    spec_test = data["specs_test"]
-    lbls_test= data["test_lbls"]
-    spec_test = (spec_test.astype(np.float32) - 127.5) / 127.5
-    
-    return face_train,\
-        spec_train,\
-        lbls_train,\
-        face_test,\
-        spec_test,\
-        lbls_test
+           
+            trn_fts= _dt_["face_train"][:self._lmt_1]
+            trn_trg = _dt_["specs_train"][:self._lmt_1]
+            trn_lbls = _dt_["train_lbls"][:self._lmt_1]
+            trn_trg = _norm_(trn_trg)
 
+            
+            vld_fts = _dt_["face_train"][self._lmt_2:self._lmt_3]
+            vld_trg = _dt_["specs_train"][self._lmt_2:self._lmt_3]
+            vld_lbls = _dt_["train_lbls"][self._lmt_2:self._lmt_3]
+            
+            vld_trg = _norm_(vld_trg)
 
-def gen_data_iwGANs(generator, 
-        face_train, 
-        audio_train, 
-        face_test, 
-        audio_test, 
-        lbls_train, 
-        lbls_test):
+            tst_fts = _dt_["face_train"][self._lmt_4:]
+            tst_trg = _dt_["specs_train"][self._lmt_4:]
+            tst_lbls= _dt_["train_lbls"][self._lmt_4:]
+            tst_trg = _norm_(tst_trg)
+            
+            randomize = np.arange(len(trn_fts))
+            np.random.shuffle(randomize)
+            trn_fts = trn_fts[randomize]
+            trn_trg = trn_trg[randomize]
+            trn_lbls = trn_lbls[randomize]
 
-        from sklearn.utils import shuffle
-        generator.load_weights('models/iwgans_gen_specs_')
+            trn_lbls = self._sft_crisp_lbl_(trn_lbls)
+            tst_lbls = self._sft_crisp_lbl_(tst_lbls)
+            vld_lbls = self._sft_crisp_lbl_(vld_lbls)
+            
+            _dct_ = {"trn_fts": trn_fts, 
+                "trn_trg": trn_trg, 
+                "trn_lbls": trn_lbls,
+                "vld_fts": vld_fts, 
+                "vld_trg": vld_trg, 
+                "vld_lbls": vld_lbls,
+                "tst_fts": tst_fts, 
+                "tst_trg": tst_trg, 
+                "tst_lbls": tst_lbls,
+                }
 
-        (face_test, audio_test, lbls_test) = shuffle(face_test, audio_test, lbls_test)
-        face_test = face_test[0:30000]
-        audio_test = audio_test[0:30000]
-        lbls_test = lbls_test[0:30000]
+            return _dct_
 
-        noise = np.random.normal(0, 1, (face_test.shape[0], 100))
-        noise = np.concatenate([noise, lbls_test], axis = 1) #noise = face_imgs
+        
+        elif target == "face":
 
-        generated_test = generator.predict([noise])
-        (face_train, audio_train, lbls_train) = shuffle(face_train, audio_train, lbls_train)
-        face_train = face_train[0:300000]
-        audio_train = audio_train[0:300000]
-        lbls_train = lbls_train[0:300000]
+            data = load_obj("models/extrAudioFeats64.pkl") 
 
-        #pdb.set_trace()
-        noise = np.random.normal(0, 1, (face_train.shape[0], 100))
-        noise = np.concatenate([noise, lbls_train], axis = 1) #noise = face_imgs
-        generated_train = generator.predict([noise])
-        generated_train = shuffle(generated_train)
+            trn_feats= data["trainFeats"]
+            trn_tgt = data["face_train"]
+            trn_lbls = data["train_lbls"]
+        
+            tst_fts= data["testFeats"]
+            tst_trg = data["face_test"]
+            
+            tst_lbls= data["test_lbls"]
+            
+            randomize = np.arange(len(trn_feats))
+            np.random.shuffle(randomize)
+            trn_feats = trn_feats[randomize]
+            trn_tgt = trn_tgt[randomize]
+            trn_lbls = trn_lbls[randomize]
 
-        pdb.set_trace()
-        gen_data_dictionary = {"gen_train": generated_train, "gen_train_lbls": lbls_train, "gen_test": generated_test, "gen_test_lbls": lbls_test}
-        store_obj("models/gen_with_wgans_sp.pkl", gen_data_dictionary)
-        real_data_dictionary = {"real_train_audio": audio_train, "real_test_audio": audio_test, "train_lbls": lbls_train, "test_lbls": lbls_test}
-        store_obj("models/real.pkl", real_data_dictionary)
+            trn_lbls = self._sft_crisp_lbl_(trn_lbls)
+            tst_lbls = self._sft_crisp_lbl_(tst_lbls)
 
+            trn_tgt = trn_tgt.transpose(0,2,3,1)
+            tst_trg = tst_trg.transpose(0,2,3,1)
+
+            _dct_ = {"trn_feats": trn_feats, 
+                "trn_tgt": trn_tgt, 
+                "trn_lbls": trn_lbls,
+                "tst_fts": tst_fts, 
+                "tst_trg": tst_trg, 
+                "tst_lbls": tst_lbls,
+                }
+            
+            return _dct_
 
                                
 
--- my_cnn_classifier.py
+++ my_cnn_classifier.py
@@ -4,7 +4,7 @@ import os
 def cls(): os.system('clear')
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 import numpy as np
 import matplotlib
@@ -27,7 +27,7 @@ from keras import backend as K
 K.common.set_image_dim_ordering('th')
 
 
-from data_handling import *
+from data_handling import data_handle
 from keras_models import *
 import matplotlib.pyplot as plt
 
@@ -47,48 +47,48 @@ class my_cnn_classifier():
 
         self.type_of_features = "prime"
 
+        self.my_obj = data_handle()
+
 
     def load_gen_data(self, type_of_feats):
 
         data_list = []
         lbls_list = []
-        for index in range(1, 4):      
+        for index in range(0, 2):      
             dict_ = load_obj("../../GANs_models/"+type_of_feats+"_gen_audio_face_feat_"+str(index)+".pkl")            
             data = dict_["gen_train"] 
             lbls_list.append(dict_["lbls_train"])       
             data_list.append(data.transpose(0, 3, 1, 2))
         
-        return data_list[0], data_list[1], data_list[2], lbls_list[0], lbls_list[1], lbls_list[2]
+        return data_list[0], data_list[1], lbls_list[0], lbls_list[1]
 
 
     def run_classifier(self):
-
-        (train_feats, train_target, lbls_train, valid_feats, valid_target, lbls_valid, test_feats, test_target, lbls_test) \
-                = load_3d_dataset("audio")
-
-        train_target = train_target.transpose(0, 3, 1, 2)[:150000]
-        test_target = test_target.transpose(0, 3, 1, 2)
-
-        gen_1, gen_2, gen_3, gen_lbls_1, gen_lbls_2, gen_lbls_3 = self.load_gen_data(self.type_of_features)
         
-        #pdb.set_trace()
+        _dct_  = self.my_obj.load_3d_dataset("audio")
 
-        #train_data = np.concatenate((train_target,  gen_1), axis=0)
-        #train_labels = np.concatenate((lbls_train[:150000,0:6], gen_lbls_1), axis=0)
+        train_target = _dct_["trn_trg"].transpose(0, 3, 1, 2)
+        test_target = _dct_["tst_trg"].transpose(0, 3, 1, 2)
+
         
-        train_data = train_target
-        train_labels = lbls_train[:150000,0:6]
+        gen_1, gen_2, gen_lbls_1, gen_lbls_2 = self.load_gen_data(self.type_of_features)
+
+        train_target = train_target[:(gen_1.shape[0]+ gen_1.shape[1])] #.train_data
+        train_labels = _dct_["trn_lbls"][:(gen_1.shape[0]+ gen_1.shape[1])]
+
+        train_data = np.concatenate((train_target,  gen_1, gen_2), axis=0)
+        train_labels = np.concatenate((train_labels, gen_lbls_1, gen_lbls_2), axis=0)
+        
+        #train_data = train_target
+        #train_labels = _dct_["trn_lbls"][:(gen_1.shape[0]+ gen_1.shape[1])]
         
         test_data = test_target 
-        test_labels = lbls_test[:,0:6]
-        
-        del train_feats
-        del train_target
-        del valid_target
-        del test_target
-        del gen_1, gen_2, gen_3
+        test_labels = _dct_["tst_lbls"]
 
-        #pdb.set_trace()
+        pdb.set_trace()
+        del _dct_
+        del gen_1, gen_2
+
         # Find the unique numbers from the train labels  
         print('Total number of outputs : ', self.nClasses)
         # Find the shape of input images and create the variable input_shape
@@ -104,17 +104,16 @@ class my_cnn_classifier():
         
         total_loss = []
         total_cm = []
-        pdb.set_trace()
         
         for iteration in range(0,4):
             model.compile(optimizer=self.c_optim, loss='categorical_crossentropy', metrics=['accuracy'])
             history = model.fit(train_data, train_labels, batch_size=self.batch_size, epochs = self.epochs, verbose=1, validation_data=(train_data, train_labels))      
             
-            c_loss = model.evaluate(test_data, lbls_test[:,0:6]) 
+            c_loss = model.evaluate(test_data, test_labels) 
             print (c_loss)
             y_pred = model.predict(test_data)
             total_loss.append(c_loss)      
-            cnf_matrix = self.plot_confusion_matrix(y_pred, lbls_test[:,0:6], iteration)
+            cnf_matrix = self.plot_confusion_matrix(y_pred, test_labels, iteration)
             total_cm.append(cnf_matrix)
             pdb.set_trace()
 
